7b5c9c233b4cc9db095ce447a2d89c7bfd5590fd
Sedat Yeni <sedat.yeni@bertelsmann.de>
2026-07-06 13:40:41 +0000

    [EPIC21-T2] Admin Portal - split into separate apps: Branches, AuditLogs, Payments extracted to their own Fiori apps, fixing the broken multi-entity List Report defect (cap-notes #12)

---
+++ app/admin-auditlogs/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("automarket.adminauditlogs.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
