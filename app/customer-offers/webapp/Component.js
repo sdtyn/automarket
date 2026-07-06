b49c66b587eeee8803cc95bb80c5c350ca633a75
Sedat Yeni <sedat.yeni@bertelsmann.de>
2026-07-06 13:40:47 +0000

    [EPIC21-T3] Customer Portal - split into separate apps: Reservations, Offers, TestDrives, Orders, Payments extracted to their own Fiori apps, fixing the broken multi-entity List Report defect (cap-notes #12)

---
+++ app/customer-offers/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("automarket.customeroffers.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
---
+++ app/customer-testdrives/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("automarket.customertestdrives.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
